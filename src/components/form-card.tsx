e389ee3c76934d9aff429a87c63abf59d95c3d47
Wirlhawk <rayhantogarlumbantobing@gmail.com>
2025-05-09 05:50:18 +0700

    gg

--- src/components/form-card.tsx
+++ src/components/form-card.tsx
@@ -8,15 +8,25 @@ import {
     CardTitle,
 } from "@/components/ui/card";
 import FormBuilder from "./form-builder";
+import Image from "next/image";
 
 export default function FormCard() {
     return (
-        <Card className="max-w-[22rem] shadow-active neu neu-pressed">
-            <CardHeader>
-                <CardTitle className="text-2xl">Kotak Aspirasi</CardTitle>
-                <CardDescription>
-                    Lorem, ipsum dolor sit amet consectetur adipisicing{" "}
-                </CardDescription>
+        <Card className="w-[22rem] shadow-active neu neu-pressed">
+            <CardHeader className="flex flex-row gap-3">
+                <Image
+                    src="/alamanah.png"
+                    alt=""
+                    width={200}
+                    height={200}
+                    className="size-16 my-auto"
+                />
+                <div>
+                    <CardTitle className="text-2xl">Kotak Aspirasi</CardTitle>
+                    <CardDescription>
+                        Lorem, ipsum dolor sit amet consectetur adipisicing{" "}
+                    </CardDescription>
+                </div>
             </CardHeader>
             <CardContent>
                 <FormBuilder />
@@ -24,3 +34,17 @@ export default function FormCard() {
         </Card>
     );
 }
+
+// export default function FormCard() {
+//     return (
+//         <div className="w-[22rem] bg-card neu neu-pressed border-black rounded-2xl">
+//             <div className="bg-green-400 p-5 text-center font-bold rounded-t-2xl">
+//                 <h1 className="text-xl">Sampaikan Aspirasimu Disini</h1>
+//             </div>
+
+//             <div className="p-5">
+//                 <FormBuilder />
+//             </div>
+//         </div>
+//     );
+// }
--- src/components/ui/input.tsx
+++ src/components/ui/input.tsx
@@ -12,7 +12,7 @@ const Input = React.forwardRef<HTMLInputElement, React.ComponentProps<"input">>(
                 //                 className
                 //             )}
                 className={cn(
-                    "flex h-11 w-full rounded-xl border-[3px] border-foreground bg-background px-3 py-2 text-base shadow-sm transition-colors file:border-0 file:bg-transparent file:text-sm file:font-medium file:text-foreground placeholder:text-muted-foreground  focus-visible:ring-ring disabled:cursor-not-allowed disabled:opacity-50 md:text-sm",
+                    "flex h-11 w-full rounded-xl border-[3px] border-foreground bg-background px-3 py-2 text-base shadow-sm transition-colors file:border-0 file:bg-transparent file:text-sm file:font-medium file:text-foreground placeholder:text-muted-foreground  focus-visible:ring-ring disabled:cursor-not-allowed disabled:opacity-50 md:text-sm font-bold",
                     className
                 )}
                 ref={ref}
